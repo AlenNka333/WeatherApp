0e24e99b2547253af4b279fe5cf320983c31a97f
Alena <elena.walst@bk.ru>
2020-07-17 19:07:10 +0300

    Commit classes
    WEAT-4

--- WeatherApp/MainScreen/MainScreenViewController.swift
+++ WeatherApp/MainScreen/MainScreenViewController.swift
@@ -10,21 +10,41 @@ import UIKit
 
 class MainScreenViewController: UIViewController {
 
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var lastUpdateTimeLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var sunriseView: WeatherParamView!
+    @IBOutlet weak var sunsetView: WeatherParamView!
+    @IBOutlet weak var windView: WeatherParamView!
+    @IBOutlet weak var humidityView: WeatherParamView!
+    @IBOutlet weak var pressureView: WeatherParamView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        makeGradientBackground()
+    
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func goToForecastAction(_ sender: Any) {
+        
     }
-    */
+}
 
+extension MainScreenViewController {
+    
+    func makeGradientBackground() {
+        let colorTop =  UIColor(red: 49.0/255.0, green: 27.0/255.0, blue: 160.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 179.0/255.0, green: 157.0/255.0, blue: 219.0/255.0, alpha: 1.0).cgColor
+
+           let gradientLayer = CAGradientLayer()
+           gradientLayer.colors = [colorTop, colorBottom]
+           gradientLayer.locations = [0.0, 1.0]
+           gradientLayer.frame = self.view.bounds
+
+           self.view.layer.insertSublayer(gradientLayer, at:0)
+           
+       }
+    
 }
